2cc37049662d2eee36593b40f82173c8c25b8365
Jacob Sinclair <jcbsnclr@outlook.com>
2023-04-09 18:35:18 +0100

    add human gtk theme

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ cp -r config/* $HOME/.config/
 cp tmux.conf $HOME/.tmux.conf
 cp profile $HOME/.profile
 cp ashrc $HOME/.ashrc
-cp -r themes/ ~/.themes/
+cp -r themes/* ~/.themes/
 
 mkdir -p $HOME/net/down $HOME/net/share 
 mkdir -p $HOME/docs/templates 
 mkdir -p $HOME/media/music $HOME/media/videos $HOME/media/pictures
 
-cp -r wallpapers/ $HOME/media/pictures/wallpapers
+cp -r wallpapers/* $HOME/media/pictures/wallpapers
--- profile
+++ profile
@@ -5,6 +5,10 @@ if [ "$XDG_SESSION_TYPE" == "wayland" ]; then
     export MOZ_ENABLE_WAYLAND=1
 fi
 
+FREETYPE_PROPERTIES="truetype:interpreter-version=35"
+GTK_OVERLAY_SCROLLING=0
+GT_QPA_PLATFORMTHEME=gtk2
+
 if test -z "$XDG_RUNTIME_DIR"; then
     export XDG_RUNTIME_DIR=/tmp/$(id -u)-rt-dir
     if ! test -d "$XDG_RUNTIME_DIR"; then
