e3a083b6fb5e451a5ddad196d1c9424c93269d23
Jacob Sinclair <jcbsnclr@outlook.com>
2023-04-09 17:46:14 +0100

    add Obatross theme, switch from Makefile to shell script decided that a Makefile would be overkill because I don't use any make features lol

--- Makefile
+++
@@ -1,5 +0,0 @@
-install:
-	cp -r config/* $(HOME)/.config/
-	cp tmux.conf $(HOME)/.tmux.conf
-	cp profile $(HOME)/.profile
-	cp ashrc $(HOME)/.ashrc
---
+++ install.sh
@@ -0,0 +1,5 @@
+cp -r config/* $HOME/.config/
+cp tmux.conf $HOME/.tmux.conf
+cp profile $HOME/.profile
+cp ashrc $HOME/.ashrc
+cp -r themes/ ~/.themes/
