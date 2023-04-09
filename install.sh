f635934d676caf63ae0df263e5dfa97edd2b54a3
Jacob Sinclair <jcbsnclr@outlook.com>
2023-04-09 18:04:43 +0100

    add wallpaper, set up user dirs on install

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+#!/usr/bin/env sh
 cp -r config/* $HOME/.config/
 cp tmux.conf $HOME/.tmux.conf
 cp profile $HOME/.profile
 cp ashrc $HOME/.ashrc
 cp -r themes/ ~/.themes/
+
+mkdir -p $HOME/net/down $HOME/net/share 
+mkdir -p $HOME/docs/templates 
+mkdir -p $HOME/media/music $HOME/media/videos $HOME/media/pictures
+
+cp -r wallpapers/ $HOME/media/pictures/wallpapers
